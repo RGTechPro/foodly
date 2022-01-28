ac400280fd6020ed28116f6669621b0e0f0aae93
RGTechPro <73791635+RGTechPro@users.noreply.github.com>
2022-01-28 20:45:31 +0530

    added signin with google

--- lib/Services/auth_services.dart
+++ lib/Services/auth_services.dart
@@ -1,1 +1,65 @@
-class AuthServices {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Auth extends ChangeNotifier {
+   User? _firebaseUser; 
+   bool isLoading = false;
+  setUser(User? user) {
+    _firebaseUser = user;
+    notifyListeners();
+  }
+   User? getUser() {
+    return _firebaseUser;
+  }
+ Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+        isLoading = true;
+    //isLoading = true;
+    notifyListeners();
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+ addUser() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    bool exist = false;
+    try {
+      await users.doc(user!.uid).get().then((doc) {
+        exist = doc.exists;
+        if (exist == true) print('exist');
+      });
+    } catch (e) {
+      // If any error
+      exist = false;
+      print(e);
+    }
+    if (exist == false) {
+      users
+          .doc(user!.uid)
+          .set({
+            'name': user.displayName,
+            'email': user.email,
+            'phoneNo': user.phoneNumber
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+    }
+  }
+
+
+}
--- lib/UI_Screens/log_in.dart
+++ lib/UI_Screens/log_in.dart
@@ -1,30 +1,81 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:schaffen_task/Components/button.dart';
 import 'package:schaffen_task/Constants/size_config.dart';
+import 'package:schaffen_task/Services/auth_services.dart';
 import 'package:schaffen_task/UI_Screens/Catalogue-Screen/catalogue.dart';
 import 'package:schaffen_task/UI_Screens/Deliver-Address/address.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
   const LogIn({Key? key}) : super(key: key);
   static String routeName = "/log_in";
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
       body: Center(
-        child: SizedBox(
-          height: SizeConfig.screenHeight! * 0.08,
-          width: SizeConfig.screenWidth! * 0.6,
-          child: SignInButton(
-            Buttons.GoogleDark,
-            onPressed: () {
-              Navigator.pushNamed(
-                context,
-                CatalogueScreen.routeName,
-              );
-            },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'FOODLY',
+              style: TextStyle(
+                  fontSize: 55,
+                  color: Colors.brown,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              height: SizeConfig.screenHeight! * 0.08,
+              width: SizeConfig.screenWidth! * 0.6,
+              child: !Provider.of<Auth>(context, listen: false).isLoading
+        ? SignInButton(
+                Buttons.GoogleDark,
+                onPressed: ()async {
+
+  try {
+                UserCredential googleUserCred =
+                    await Provider.of<Auth>(context, listen: false)
+                        .signInWithGoogle();
+                Provider.of<Auth>(context, listen: false)
+                    .setUser(googleUserCred.user);
+                Provider.of<Auth>(context, listen: false).addUser();
+                if (Provider.of<Auth>(context, listen: false).getUser() !=
+                    null) {
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+          Navigator.pushNamed(
+                    context,
+                    CatalogueScreen.routeName,
+                  );
+                  final snackBar = SnackBar(content: Text('Logged In!'));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+                setState(() {
+                  Provider.of<Auth>(context, listen: false).isLoading = false;
+                });
+              } catch (e) {
+                setState(() {
+                  Provider.of<Auth>(context, listen: false).isLoading = false;
+                });
+                ;
+              }
+                  
+                },
+              ): CircularProgressIndicator(
+            color: Colors.redAccent,
           ),
+            )
+          ],
         ),
       ),
     );
